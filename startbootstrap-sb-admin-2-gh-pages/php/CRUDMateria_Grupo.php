20b2d702050345cdd8064588e40eb77c7962b717
EsterUscanga <ester.uscanga@cceo.com.mx>
2019-03-02 19:25:18 -0600

    Was added > to each select to CRUDMateria_Grupo.php

--- startbootstrap-sb-admin-2-gh-pages/php/CRUDMateria_Grupo.php
+++ startbootstrap-sb-admin-2-gh-pages/php/CRUDMateria_Grupo.php
@@ -2,7 +2,7 @@
   include 'connetion.php';
   $optionMateria = "SELECT * FROM materias order by nombre_materia asc";
   $resultMateria = mysqli_query($conn, $optionMateria);
-  $selectMateria = '<select id="selectMateria" class="form-control"'; 
+  $selectMateria = '<select id="selectMateria" class="form-control">'; 
   while ($row = mysqli_fetch_array($resultMateria)){
     $selectMateria .= '<option value="" id=" '. $row['id_materia'] .'">' . $row['nombre_materia'] . '</option>';
   }
@@ -10,7 +10,7 @@
 
   $optionMaestro = "SELECT * FROM maestros order by apellido_maestro asc";
   $resultMaestro = mysqli_query($conn, $optionMaestro);
-  $selectMaestro = '<select id="selectProfesor" class="form-control"'; 
+  $selectMaestro = '<select id="selectProfesor" class="form-control">'; 
   while ($row = mysqli_fetch_array($resultMaestro)){
     $selectMaestro .= '<option value="" id=" '. $row['id_maestro'] .'">' . $row['nombre_maestro'] . ' '. $row['apellido_maestro'] .'</option>';
   }
