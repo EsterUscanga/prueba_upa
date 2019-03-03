ecc163603536c80f1a064a785c4f83bffa3f25f5
EsterUscanga <ester.uscanga@cceo.com.mx>
2019-03-02 18:19:27 -0600

    Was mixed horarios with materias grupos

---
+++ startbootstrap-sb-admin-2-gh-pages/pages/layout/horario.php
@@ -0,0 +1,325 @@
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-12">
+      <h1 class="page-header">Horarios</h1>
+      <table class="table table-hover table-dark">
+        <thead>
+          <tr>
+            <th scope="col">Hora</th>
+            <th scope="col">Lunes</th>
+            <th scope="col">Martes</th>
+            <th scope="col">Miercoles</th>
+            <th scope="col">Jueves</th>
+            <th scope="col">Viernes</th>
+            <th scope="col">Sabado</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">7:00 - 8:00</th>
+            <td>
+              <input type="checkbox" id="lun1">
+            </td>
+            <td>
+              <input type="checkbox" id="mar1">
+            </td>
+            <td>
+              <input type="checkbox" id="mier1">
+            </td>
+            <td>
+              <input type="checkbox" id="jue1">
+            </td>
+            <td>
+              <input type="checkbox" id="vie1">
+            </td>
+            <td>
+              <input type="checkbox" id="sab1">
+            </td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <th scope="row">8:00 - 9:00</th>
+            <div id="2">
+              <td>
+                <input type="checkbox" id="lun1">
+              </td>
+              <td>
+                <input type="checkbox" id="mar1">
+              </td>
+              <td>
+                <input type="checkbox" id="mier1">
+              </td>
+              <td>
+                <input type="checkbox" id="jue1">
+              </td>
+              <td>
+                <input type="checkbox" id="vie1">
+              </td>
+              <td>
+                <input type="checkbox" id="sab1">
+              </td>
+            </div>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <th scope="row">9:00 - 10:00</th>
+            <div id="3">
+              <td>
+                <input type="checkbox" id="lun1">
+              </td>
+              <td>
+                <input type="checkbox" id="mar1">
+              </td>
+              <td>
+                <input type="checkbox" id="mier1">
+              </td>
+              <td>
+                <input type="checkbox" id="jue1">
+              </td>
+              <td>
+                <input type="checkbox" id="vie1">
+              </td>
+              <td>
+                <input type="checkbox" id="sab1">
+              </td>
+
+            </div>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <th scope="row">10:00 - 11:00</th>
+            <div id="4">
+              <td>
+                <input type="checkbox" id="lun1">
+              </td>
+              <td>
+                <input type="checkbox" id="mar1">
+              </td>
+              <td>
+                <input type="checkbox" id="mier1">
+              </td>
+              <td>
+                <input type="checkbox" id="jue1">
+              </td>
+              <td>
+                <input type="checkbox" id="vie1">
+              </td>
+              <td>
+                <input type="checkbox" id="sab1">
+              </td>
+
+            </div>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <th scope="row">11:00 - 12:00</th>
+            <div id="5">
+              <td>
+                <input type="checkbox" id="lun1">
+              </td>
+              <td>
+                <input type="checkbox" id="mar1">
+              </td>
+              <td>
+                <input type="checkbox" id="mier1">
+              </td>
+              <td>
+                <input type="checkbox" id="jue1">
+              </td>
+              <td>
+                <input type="checkbox" id="vie1">
+              </td>
+              <td>
+                <input type="checkbox" id="sab1">
+              </td>
+            </div>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <th scope="row">13:00 - 14:00</th>
+            <div id="6">
+              <td>
+                <input type="checkbox" id="lun1">
+              </td>
+              <td>
+                <input type="checkbox" id="mar1">
+              </td>
+              <td>
+                <input type="checkbox" id="mier1">
+              </td>
+              <td>
+                <input type="checkbox" id="jue1">
+              </td>
+              <td>
+                <input type="checkbox" id="vie1">
+              </td>
+              <td>
+                <input type="checkbox" id="sab1">
+              </td>
+            </div>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <th scope="row">14:00 - 15:00</th>
+            <div id="7">
+              <td>
+                <input type="checkbox" id="lun1">
+              </td>
+              <td>
+                <input type="checkbox" id="mar1">
+              </td>
+              <td>
+                <input type="checkbox" id="mier1">
+              </td>
+              <td>
+                <input type="checkbox" id="jue1">
+              </td>
+              <td>
+                <input type="checkbox" id="vie1">
+              </td>
+              <td>
+                <input type="checkbox" id="sab1">
+              </td>
+            </div>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <th scope="row">15:00 - 16:00</th>
+            <div id="8">
+              <td>
+                <input type="checkbox" id="lun1">
+              </td>
+              <td>
+                <input type="checkbox" id="mar1">
+              </td>
+              <td>
+                <input type="checkbox" id="mier1">
+              </td>
+              <td>
+                <input type="checkbox" id="jue1">
+              </td>
+              <td>
+                <input type="checkbox" id="vie1">
+              </td>
+              <td>
+                <input type="checkbox" id="sab1">
+              </td>
+            </div>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <th scope="row">17:00 - 18:00</th>
+            <div id="9">
+              <td>
+                <input type="checkbox" id="lun1">
+              </td>
+              <td>
+                <input type="checkbox" id="mar1">
+              </td>
+              <td>
+                <input type="checkbox" id="mier1">
+              </td>
+              <td>
+                <input type="checkbox" id="jue1">
+              </td>
+              <td>
+                <input type="checkbox" id="vie1">
+              </td>
+              <td>
+                <input type="checkbox" id="sab1">
+              </td>
+            </div>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <th scope="row">18:00 - 19:00</th>
+            <div id="10">
+              <td>
+                <input type="checkbox" id="lun1">
+              </td>
+              <td>
+                <input type="checkbox" id="mar1">
+              </td>
+              <td>
+                <input type="checkbox" id="mier1">
+              </td>
+              <td>
+                <input type="checkbox" id="jue1">
+              </td>
+              <td>
+                <input type="checkbox" id="vie1">
+              </td>
+              <td>
+                <input type="checkbox" id="sab1">
+              </td>
+            </div>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <th scope="row">19:00 - 20:00</th>
+            <div id="11">
+              <td>
+                <input type="checkbox" id="lun1">
+              </td>
+              <td>
+                <input type="checkbox" id="mar1">
+              </td>
+              <td>
+                <input type="checkbox" id="mier1">
+              </td>
+              <td>
+                <input type="checkbox" id="jue1">
+              </td>
+              <td>
+                <input type="checkbox" id="vie1">
+              </td>
+              <td>
+                <input type="checkbox" id="sab1">
+              </td>
+            </div>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <th scope="row">20:00 - 21:00</th>
+            <div id="12">
+              <td>
+                <input type="checkbox" id="lun1">
+              </td>
+              <td>
+                <input type="checkbox" id="mar1">
+              </td>
+              <td>
+                <input type="checkbox" id="mier1">
+              </td>
+              <td>
+                <input type="checkbox" id="jue1">
+              </td>
+              <td>
+                <input type="checkbox" id="vie1">
+              </td>
+              <td>
+                <input type="checkbox" id="sab1">
+              </td>
+            </div>
+          </tr>
+        </tbody>
+
+      </table>
+    </div>
+
+    <!-- /.col-lg-12 -->
+  </div>
+  <!-- /.row -->
+</div>
+<!-- /.container-fluid -->
